1860d554ef76666898552fc0d21fbc8c928fa4ec
Kwon Yoo <carlosyu75@gmail.com>
2018-08-19 19:31:11 -0700

    Add project files.

---
+++ AspnetNote/DataContext/AspnetNoteDbContext.cs
@@ -0,0 +1,22 @@
+﻿using AspnetNote.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspnetNote.DataContext
+{
+    public class AspnetNoteDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Note> Notes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=localhost;Initial Catalog=AspnetNote;Integrated Security=True");
+
+        }
+    }
+}
---
+++ AspnetNote/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspnetNote.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserNo { get; set; }
+
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+      
+        [Required]
+        public string UserPassword{ get; set; }
+    }
+}
