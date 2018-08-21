6fca3f45bd8c2ecf422b6c8b0af4a69e7aa5a611
Kwon Yoo <carlosyu75@gmail.com>
2018-08-21 14:18:50 -0700

    Register add about valid state

--- AspnetNote/Controllers/AccountController.cs
+++ AspnetNote/Controllers/AccountController.cs
@@ -2,20 +2,39 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AspnetNote.DataContext;
+using AspnetNote.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AspnetNote.Controllers
 {
     public class AccountController : Controller
     {
+        [HttpGet]
         public IActionResult Login()
         {
             return View();
         }
 
+        [HttpGet]
         public IActionResult Register()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(User model)
+        {
+            if (ModelState.IsValid)
+            {
+                using(var db = new AspnetNoteDbContext())
+                {
+                    db.Users.Add(model);
+                    db.SaveChanges();
+                }
+                return RedirectToAction("Index", "Home");
+            }
+            return View(model);
         }
     }
 }
