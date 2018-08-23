dffedea522a4b46bf92086a13f9de494ddf9c3a8
Kwon Yoo <carlosyu75@gmail.com>
2018-08-23 14:39:23 -0700

    Add session logout

---
+++ AspnetNote/Controllers/NoteController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspnetNote.DataContext;
+using AspnetNote.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspnetNote.Controllers
+{
+    public class NoteController : Controller
+    {
+        /// <summary>
+        /// Note List
+        /// </summary>
+        /// <returns></returns>
+        /// CRUD Funtion method
+        public IActionResult Index()
+        {
+            using (var db = new AspnetNoteDbContext())
+            {
+                var list = db.Notes.ToList();
+                return View(list);
+            }
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        public IActionResult Edit()
+        {
+            return View();
+        }
+
+        public IActionResult Delete()
+        {
+            return View();
+        }
+    }
+}
